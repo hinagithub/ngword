22bb90ee8ca2a2db71c3d12a300a9e9711fbac10
hinako <matsuno.hinako@gmail.com>
2020-06-21 16:05:29 +0900

    migration設定完了

--- seeders/20200621063008-test-ngwords.js
+++ seeders/20200621063008-test-ngwords.js
@@ -2,25 +2,85 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('ngwords', [
+      {
+        name: '青いもの(例: 空、海)',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '世界遺産',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '食べ物',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '漫画や小説の題名',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '三十秒間笑わない',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '昔話のキャラクター(例: 桃太郎、かぐや姫)',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '都道府県や市区町村名',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '白いもの(例: 雪、雲)',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '否定をする',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'カフェの名前',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '乗り物',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '誰か一人でも知らないこと',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '黒いもの(ex.ゴキブリ、宇宙)',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: '布でできているもの',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'IT機器(例: スマホ、パソコン)',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
-
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+    return queryInterface.bulkDelete('Rooms', null, {});
+  },
 };
--- seeders/20200621063008-test-players.js
+++ seeders/20200621063008-test-players.js
@@ -2,25 +2,28 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('players', [
+      {
+        room_id: 2,
+        name: 'わっちゃん',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        room_id: 2,
+        name: 'さち',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        room_id: 2,
+        name: 'ゆりゆり',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
-
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+    return queryInterface.bulkDelete('Rooms', null, {});
+  },
 };
