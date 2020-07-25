fcf532d5df7628d731701f61ffd0d6b162f9b671
hinako <matsuno.hinako@gmail.com>
2020-07-25 17:44:30 +0900

    bootstrap導入

--- routes/makeRoom.js
+++ routes/makeRoom.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const _ = require('lodash');
+const { sequelize } = require('../models');
 const Room = require('../models').rooms;
 const Player = require('../models').players;
 const Ngword = require('../models').ngwords;
@@ -13,31 +14,41 @@ router.get('/', function (req, res, next) {
 
 /* POST makeRoom*/
 router.post('/', async function (req, res, next) {
-  // ランダムな部屋名を生成しDBに登録
+  // トランザクション開始
+  const t = await sequelize.transaction();
   const roomName = Math.random().toString(32).substring(2);
-  const room = await Room.create({ name: roomName });
-
-  // プレイヤー名を取得し登録
-  const playerRegisterInfo = req.body.player.map((player) => {
-    return {
-      room_id: room.id,
-      name: player,
-    };
-  });
-  await Player.bulkCreate(playerRegisterInfo);
-
-  // 全NGワードを取得
-  const playerCount = req.body.player.length;
-  const allNgwords = await Ngword.findAll();
-  // シャッフルしてプレイヤー人数の分だけ取得
-  const ngwordsInfo = _.shuffle(allNgwords)
-    .splice(0, playerCount)
-    .map((word) => {
-      return { room_id: room.id, ngword_id: word.id };
+  try {
+    // ランダムな部屋名を生成しDBに登録
+    const room = await Room.create({ name: roomName }, { transaction: t });
+    // プレイヤー名を取得し登録
+    const playerRegisterInfo = req.body.player.map((player) => {
+      return {
+        room_id: room.id,
+        name: player,
+      };
     });
-  // NGワードを登録
-  await NgwordRoom.bulkCreate(ngwordsInfo);
+    await Player.bulkCreate(playerRegisterInfo, { transaction: t });
+    // 全NGワードを取得
+    const playerCount = req.body.player.length;
+    const allNgwords = await Ngword.findAll({ transaction: t });
 
+    // シャッフルしてプレイヤー人数の分だけNGワードを生成&登録
+    const ngwordsInfo = _.shuffle(allNgwords)
+      .splice(0, playerCount)
+      .map((word) => {
+        return { room_id: room.id, ngword_id: word.id };
+      });
+    await NgwordRoom.bulkCreate(ngwordsInfo, { transaction: t });
+
+    // コミット
+    await t.commit();
+
+    // エラーの場合の処理
+  } catch (error) {
+    await t.rollback();
+    const errMessage = 'なんらかのエラーで部屋を作れませんでした...。';
+    res.render('makeRoomFailed', { errMessage: errMessage });
+  }
   // 成功画面へ遷移
   res.render('makeRoomSuccess', { roomName: roomName });
 });
